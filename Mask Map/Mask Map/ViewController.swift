dc97cb98f0439ec693adecfa193d03a919752ce1
wyn <k022298@gmail.com>
2020-03-03 22:33:32 +0800

    creat project

---
+++ Mask Map/Mask Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mask Map
+//
+//  Created by wyn on 2020/3/3.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
