dfc709fb51a38d973075a59fd04e3f0f810e274e
wyn <k022298@gmail.com>
2020-03-05 23:34:02 +0800

    add a masktableviewcell

--- MaskMap/MaskMap/ViewController.swift
+++ MaskMap/MaskMap/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let url = URL(string: "https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1") {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                if let data = data, let masks = try? decoder.decode([Mask].self, from: data) {
+                    print(masks)
+                }
+            }.resume()
+        }
     }
 
 
---
+++ MaskMap/MaskMap/maskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  MaskMap
+//
+//  Created by wyn on 2020/3/5.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class maskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
