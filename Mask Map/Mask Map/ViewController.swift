ca7b53f97cd36c054e543f475129db88e00df6f6
wyn <k022298@gmail.com>
2020-03-05 21:51:16 +0800

    adding datatableveiwcontroller

--- Mask Map/Mask Map/ViewController.swift
+++ Mask Map/Mask Map/ViewController.swift
@@ -8,46 +8,13 @@
 
 import UIKit
 
-struct Mask: Decodable {
-    let id: String
-    let name: String
-    let address: String
-    let tel: String
-    let adultCount: String
-    let childCount: String
-    let time: Date
-    
-    enum CodingKeys: String, CodingKey  {
-        case id = "醫事機構代碼"
-        case name = "醫事機構名稱"
-        case address = "醫事機構地址"
-        case tel = "醫事機構電話"
-        case adultCount = "成人口罩剩餘數"
-        case childCount = "兒童口罩剩餘數"
-        case time = "來源資料時間"
-    }
-}
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Hit the API endpoint
-        if let url = URL(string: "https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1") {
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                let decoder = JSONDecoder()
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-                decoder.dateDecodingStrategy = .formatted(dateFormatter)
-                if let data = data, let masks = try? decoder.decode([Mask].self, from: data) {
-                    print(masks)
-                }
-            }.resume()
-        }
-    }
-    
 
+    }
 }
 
